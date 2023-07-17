0271a59bae15034dbc6eba37e08f86014e7611ca
vlj2007 <vlj2007@yandex.ru>
2023-07-17 20:47:26 +0700

    init_git

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+//        task2();
+    }
+
+    public static void task1(){
+        System.out.println("Задача 1");
+
+    }
+
+
+}
