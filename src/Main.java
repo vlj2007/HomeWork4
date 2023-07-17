012ad0630cfd52f3ed631cb8bbb2e3d912d31b70
vlj2007 <vlj2007@yandex.ru>
2023-07-17 21:17:46 +0700

    I_have_finished_task1

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,12 @@ public class Main {
 
     public static void task1(){
         System.out.println("Задача 1");
-
+        int age = 18;
+        System.out.println("Человеку " + age);
+        if(age >= 18){
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека меньше " + age + " , то он не достиг совершеннолетия, нужно немного подождать.");
+        }
     }
-
-
 }
